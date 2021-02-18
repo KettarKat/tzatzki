caa49956afc9c0c990be2c33a4c2635ae3b13f18
KettarKat <kettarkat@gmail.com>
2021-02-18 20:53:28 +0300

    added sticky header

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,17 @@ $(function(){
     prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/arrow-left.svg" alt=""></button>',
     nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/arrow-right.svg" alt=""></button>',
   });
+  document.body.addEventListener('scroll', function() {
+    var el = $('header');
+    var className = 'fixed';
+    if (document.body.scrollTop > 131 ) {
+    if (!el.hasClass(className))
+    el.addClass(className);
+    }
+    else {
+    if (el.hasClass(className))
+    el.removeClass(className);
+    }
+    });
+    
 })
