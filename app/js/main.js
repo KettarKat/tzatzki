57a55b1b83b19514d20ff73dabb4043161b4c755
KettarKat <kettarkat@gmail.com>
2021-03-09 18:30:12 +0300

    added care page

--- app/js/main.js
+++ app/js/main.js
@@ -5,9 +5,19 @@ $(function(){
     slidesToShow: 4,
     slidesToScroll: 4,
     infinite: true,
-    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/arrow-left.svg" alt=""></button>',
-    nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/arrow-right.svg" alt=""></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/arrow-right.svg" data-bw="images/icons/arrow-right.svg" data-color="images/icons/arrow-hover-right.svg" alt=""></button>',
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/arrow-left.svg" data-bw="images/icons/arrow-left.svg" data-color="images/icons/arrow-hover-left.svg" alt=""></button>',
   });
+  $('.slick-arrow').on('mouseleave', function() {
+    $(this).find('img').attr('src', function(){
+        return $(this).data('bw');
+    });
+});
+$('.slick-arrow').on('mouseover', function() {
+    $(this).find('img').attr('src', function(){
+        return $(this).data('color');
+    });
+});
   document.body.addEventListener('scroll', function() {
     var el = $('header');
     var className = 'fixed';
